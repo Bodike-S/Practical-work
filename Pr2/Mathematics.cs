814f371fc9c789585cbb9fc277a5f767378a7011
Bodike-S <Lilishka7171@gmail.com>
2022-11-16 13:21:53 +0200

    PrN2 TN3

---
+++ Pr2/Mathematics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr2
+{
+    class Mathematics:Science
+    {
+        public override void Awards()
+        {
+            Console.WriteLine($"We don't need any awards, but we still can obtain The Abell Prize that nobody else can\n");
+        }
+    }
+}
